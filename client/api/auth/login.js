e608d651be70be516213932b12590e12849e8990
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-03-29 21:34:12 -0400

    Update login.js

--- client/api/auth/login.js
+++ client/api/auth/login.js
@@ -1,1 +1,10 @@
+import connectDB from '../../../utils/db';
+import User from '../../../models/User';
 
+export default async function handler(req, res) {
+  await connectDB();
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || user.password !== password) return res.status(401).json({ message: 'Invalid credentials' });
+  res.status(200).json({ message: 'Login success', uid: user.uid });
+}
